a36d4cc9833b88286fb76b11bfd436e643c8d9d1
Erin Keith <erinkeith.unr@gmail.com>
2022-01-25 17:01:13 -0800

    add play_game functionality, change mark_square parameter order, add change_player function

--- src/tictactoe.py
+++ src/tictactoe.py
@@ -8,16 +8,26 @@ class Board(object):
         self.__board_size = 3
         self.__board = [[0 for x in range(self.__board_size)] for y in range(self.__board_size)]
         self.__player_X = player.Player()
-        self.__player_Y = player.Player()
-        self.__current_player = ''
+        self.__player_O = player.Player()
+        self.__current_player = ' '
+        
+    def __str__(self):
+        return self.__board
+        
+    def change_player(self):
+        if self.__current_player = ' ':
+            self.__current_player = 'O'
+        elif self.__current_player = 'O':
+            self.__current_player = 'X'
+        elif self.__current_player = 'X':
+            self.__current_player = 'O'
 
-    def mark_square(self, column, row, player):
+    def mark_square(self, row, column):
         """
-        Marks a square at coordinate (column, row) for player
+        Marks a square at coordinate (row, column) for player
 
         :param column: (int) the 0-indexed column of the Board to mark
         :param row: (int) the 0-indexed row of the Board to mark
-        :param player: (str) the X or O representation of which player to mark in square
 
         :return: (bool) whether or not the square was marked
         """
@@ -26,15 +36,15 @@ class Board(object):
         inbounds = (row <= self.__board_size and column <= self.__board_size)
 
         if inbounds:
-            success = self.__board[row][column] == 0
+            success = self.__board[row][column] == ' '
 
         if success:
-            self.__board[row][column] = player
+            self.__board[row][column] = self.__current_player
 
             if player == 'X':
                 self.__player_X.update(row, column)
-            elif player == 'Y':
-                self.__player_Y.update(row, column)
+            elif player == 'O':
+                self.__player_O.update(row, column)
 
         return success
 
@@ -47,8 +57,8 @@ class Board(object):
         if self.__current_player == 'X':
             return self.__player_X.is_winner()
 
-        elif self.__current_player == 'Y':
-            return self.__player_Y.is_winner()
+        elif self.__current_player == 'O':
+            return self.__player_O.is_winner()
 
     def play_game(self):
         """
@@ -57,11 +67,19 @@ class Board(object):
 
         :return: (str) the letter representing the player who won
         """
-
-        pass
+        while not has_winner():
+            print(self.__board)
+            change_player()
+            row = input("Player " + self.__current_player + ", enter your row: ")
+            col = input("Player " + self.__current_player + ", enter your column: ")
+            while not mark_square(row, col):
+                print("Invalid Move)
+                row = input("Player " + self.__current_player + ", enter your row: ")
+                col = input("Player " + self.__current_player + ", enter your column: ")
+        return self.__current_player
 
 
 if __name__ == '__main__':
     board = Board()
     winner = board.play_game()
-    print("{} has won!".format(winner))
+    print(winner + " has won!")
